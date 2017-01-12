11638e95b8e64031964fe47e5f89e04cea377d5f
anthonypecchillo <anthonypecchillo@gmail.com>
2017-01-12 14:40:10 -0800

    Create archive helper functions and all helper function tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -25,17 +26,65 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function() {
+exports.readListOfUrls = function(cb) {
+  fs.readFile(exports.paths.list, function(err, data) {
+    if (err) {
+      console.log(err);
+      throw error;
+    }
+    var urls = data.toString().split('\n');
+    cb(err, urls);
+  });
 };
 
-exports.isUrlInList = function() {
+exports.isUrlInList = function(urlOfInterest, cb) {
+  exports.readListOfUrls(function(err, urls) {
+    var exists = urls.includes(urlOfInterest);
+    cb(err, exists);
+  });
 };
 
-exports.addUrlToList = function() {
+exports.addUrlToList = function(urlToAdd, cb) {
+  exports.readListOfUrls(function(err, urls) {
+    urls.push(urlToAdd);
+    console.log(urls);
+    fs.writeFile(exports.paths.list, urls.join('\n'), function(err) {
+      if (err) {
+        console.log(err);
+        throw error;
+      }
+      cb(err);
+    });
+  });
 };
 
-exports.isUrlArchived = function() {
+exports.isUrlArchived = function(urlOfInterest, cb) {
+  fs.readdir(exports.paths.archivedSites, function(err, files) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    var exists = files.includes(urlOfInterest);
+    cb(err, exists);
+  });
 };
 
-exports.downloadUrls = function() {
+exports.downloadUrls = function(urlsToDownloadInArray) {
+  urlsToDownloadInArray.forEach(function(url) {
+    console.log(url);
+    request({uri: 'http://' + url}, function(err, response, body) {
+      if (err) {
+        console.log(err);
+        throw error;
+      }
+
+      fs.appendFile(exports.paths.archivedSites + '/' + url, body, function(err) {
+        if (err) {
+          console.log(err);
+          throw error;
+        }
+        // cb(err);
+      });
+    });
+  });
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,64 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //curr directory is /web
+  console.log('__dirname is: ', __dirname, '\n');
+
+  fs.readFile(__dirname + '/public/index.html', function(err, data) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log('I read the file!\n');
+    console.log('The file is: ', '???file???');
+    console.log('The data is: ', data.toString());
+
+    fs.writeFile(path.join(path.normalize(__dirname + '/..'), '/test/testdata/sites.txt'), data, function(err) {
+      console.log('I\'m in the writeFile method now!\n');
+      console.log('The data is now: ' + data.toString());
+      console.log('I\'m writing the data into: ' + path.join(path.normalize(__dirname + '/..'), '/test/testdata/sites.txt'));
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+
+      var headers = httpHelpers.headers;
+      var method = req.method;
+      var url = req.url;
+      var statusCode;
+
+      console.log('Serving request type ' + method + ' for url ' + url);
+
+      if (method === 'GET') {
+        console.log('GET METHOD!!!!!!');
+        statusCode = 200;
+        
+        req.on('error', function(err) {
+          console.error(err);
+        });
+        req.on('data', function(chunk) {
+          //do stuff
+        });
+        req.on('end', function() {
+          res.writeHead(statusCode, {'Content-Type': 'application/JSON'});
+
+          var responseBody = {
+            headers: headers,
+            method: method,
+            url: url,
+            body: data.toString()
+          };
+          console.log('hi therererererere');
+          console.log(JSON.stringify({results: responseBody.body}));
+          res.end(JSON.stringify({results: responseBody.body}));
+        });
+      }     
+    });
+  });
+
+  // res.end(archive.paths.list);
 };
